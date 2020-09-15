c10b4a35003b5bcc38d4b1b2ef54a4d603c2b86d
Ahmed MAHFOUF <a.mahfouf@gmail.com>
2020-09-15 14:36:24 +0200

    Mise en place d'une classe modélisant un jet de 5 dés au sein du jeu Yatzy.

---
+++ java/src/main/java/fr/dojo/kata/yatzy/YatzyDiceRoll.java
@@ -0,0 +1,24 @@
+package fr.dojo.kata.yatzy;
+
+import lombok.Getter;
+
+/**
+ * Classe modélisant un jet de 5 dés au sein du jeu "Yatzy".
+ */
+@Getter
+public class YatzyDiceRoll {
+
+    private int d1;
+    private int d2;
+    private int d3;
+    private int d4;
+    private int d5;
+
+    public YatzyDiceRoll(int d1, int d2, int d3, int d4, int d5) {
+        this.d1 = d1;
+        this.d2 = d2;
+        this.d3 = d3;
+        this.d4 = d4;
+        this.d5 = d5;
+    }
+}
